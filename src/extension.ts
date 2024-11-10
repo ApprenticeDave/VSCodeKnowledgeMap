2c90808267cef8b3c6d7de75175b0689730bcd99
ApprenticeDave <36512502+ApprenticeDave@users.noreply.github.com>
2024-11-10 22:22:43 +0000

    Building out base structure.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as kx from './knowledgegraph.js';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,17 +11,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscodeknowledgemap" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscodeknowledgemap.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSCodeKnowledgeMap!');
+	const mapViewDisposable = vscode.commands.registerCommand('vscodeknowledgemap.OpenMapView', () => {
+		const panel = vscode.window.createWebviewPanel(
+            'vscodeknowledgemapOpenMapView', // Identifies the type of the webview. Used internally
+            'Knowledge Map', // Title of the panel displayed to the user
+            vscode.ViewColumn.One, // Editor column to show the new webview panel in
+            {} // Webview options. More on these later.
+        );
+
+		panel.webview.html = getMapViewContent();
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(mapViewDisposable);
 }
+
+function getMapViewContent(){
+	return '';
+};
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
