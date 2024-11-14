b9474622210fc378b2d0cefcf6a26793ebdd3995
Dave Russell <daverussell@meta.com>
2024-11-14 10:57:34 +0000

    Updated to include new references

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as kx from './knowledgegraph.js';
+import * as kx from './knowledgegraph';
+import * as fileparse from './fileParser';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
